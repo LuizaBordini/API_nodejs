d1620c3fe78d41437b32c0ad764fa7a3b3054a48
Luiza Bordini <luizabordini01@gmail.com>
2022-06-29 18:16:12 -0300

    Primeira API com node js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const routes = require('./config/routes')
+
+
+const app = express()
+
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+
+
+//Iniciar o meu servidor 
+app.listen(21262, () => {
+    console.log('Express started at http://localhost:21262')
+})
+
